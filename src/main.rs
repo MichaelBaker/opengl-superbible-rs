33cbc0570f93895aefa15c6849c593bb0d438482
Michael Baker <michaeltbaker@gmail.com>
2015-08-09 23:12:33 -0500

    Implements a tesselation shader

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use std::ffi::CString;
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
-    glfw.window_hint(WindowHint::ContextVersion(3,2));
+    glfw.window_hint(WindowHint::ContextVersion(4,1));
     glfw.window_hint(WindowHint::OpenGlForwardCompat(true));
     glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
 
@@ -24,12 +24,11 @@ fn main() {
 
     gl::load_with( |s| window.get_proc_address(s) );
 
-    let program             = create_program("src/vs.glsl", "src/fs.glsl");
+    let program             = create_program("src/vs.glsl", "src/fs.glsl", "src/tcs.glsl", "src/tes.glsl");
     let mut vao_one         = 0;
     let mut vbo_one         = 0;
     let data_one: [f32; 12] = [ 0.25, -0.25, 0.5, 1.0, -0.25, -0.25, 0.5, 1.0,  0.25, 0.25, 0.5, 1.0];
     let offset              = unsafe { gl::GetAttribLocation(program, CString::new("offset").unwrap().as_ptr()) };
-
 
     unsafe {
         gl::GenVertexArrays(1, &mut vao_one);
@@ -55,7 +54,7 @@ fn main() {
             gl::BindVertexArray(vao_one);
             gl::BindBuffer(gl::ARRAY_BUFFER, vbo_one);
             gl::BufferData(gl::ARRAY_BUFFER, 48, transmute(&data_one), gl::DYNAMIC_DRAW);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::DrawArrays(gl::PATCHES, 0, 3);
 
             window.swap_buffers();
 
@@ -64,14 +63,32 @@ fn main() {
     }
 }
 
-fn create_program(vs_path: &str, fs_path: &str) -> u32 {
+fn create_program(vs_path: &str, fs_path: &str, tcs_path: &str, tes_path: &str) -> u32 {
     unsafe {
-        let vs_shader = load_shader(vs_path, gl::VERTEX_SHADER);
-        let fs_shader = load_shader(fs_path, gl::FRAGMENT_SHADER);
+        let vs_shader  = load_shader(vs_path,  gl::VERTEX_SHADER);
+        let fs_shader  = load_shader(fs_path,  gl::FRAGMENT_SHADER);
+        let tcs_shader = load_shader(tcs_path, gl::TESS_CONTROL_SHADER);
+        let tes_shader = load_shader(tes_path, gl::TESS_EVALUATION_SHADER);
         let program = gl::CreateProgram();
         gl::AttachShader(program, vs_shader);
         gl::AttachShader(program, fs_shader);
+        gl::AttachShader(program, tcs_shader);
+        gl::AttachShader(program, tes_shader);
         gl::LinkProgram(program);
+
+        let mut status = gl::FALSE as GLint;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len: GLint = 0;
+            gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::with_capacity(len as usize);
+            buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+            gl::GetProgramInfoLog(program, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
+            panic!("{}", str::from_utf8(&buf).ok().expect("ProgramInfoLog not valid utf8"));
+        }
+
         gl::DeleteShader(vs_shader);
         gl::DeleteShader(fs_shader);
         program
@@ -93,7 +110,7 @@ fn load_shader(path: &str, shader_type: u32) -> u32 {
             let mut buf = Vec::with_capacity(len as usize);
             buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
             gl::GetShaderInfoLog(shader, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
-            panic!("{}", str::from_utf8(&buf).ok().expect("ShaderInfoLog not valid utf8"));
+            panic!("Error compiling: {}\n{}", path, str::from_utf8(&buf).ok().expect("ShaderInfoLog not valid utf8"));
         }
 
         shader
--- src/vs.glsl
+++ src/vs.glsl
@@ -1,12 +1,7 @@
-#version 150 core
+#version 410 core
 
 in vec4 offset;
 
 void main(void) {
-  const vec4 vertices[3] = vec4[3](
-      vec4(0.25, -0.25, 0.5, 1.0),
-      vec4(-0.25, -0.25, 0.5, 1.0),
-      vec4(0.25, 0.25, 0.5, 1.0));
-
   gl_Position = offset;
 }
